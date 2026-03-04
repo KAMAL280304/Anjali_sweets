7fb9648c80e1ec91871ef1efb295a2c6e51a6e76
KAMAL280304 <prabakarkamal28@gmail.com>
2026-03-04 22:23:27 +0530

    webhook.py fixed

--- minimal_server.py
+++ minimal_server.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, request
+
 app = Flask(__name__)
-from webhook import *
+
 @app.route("/ping")
 def ping():
     return "pong"
 
-if __name__ == "__main__":
-    print("Starting minimal server on 8081...")
-    app.run(host='0.0.0.0', port=8081)
+@app.route("/webhook", methods=["GET","POST"])
+def webhook():
+
+    if request.method == "GET":
+        mode = request.args.get("hub.mode")
+        token = request.args.get("hub.verify_token")
+        challenge = request.args.get("hub.challenge")
+
+        if mode == "subscribe" and token == "my_verify_token_123":
+            return challenge, 200
+        else:
+            return "Forbidden", 403
+
+    if request.method == "POST":
+        data = request.get_json()
+        print("Incoming webhook:", data)
+        return "EVENT_RECEIVED", 200
